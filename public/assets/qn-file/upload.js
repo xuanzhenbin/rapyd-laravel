df3f1fbd2ec5efecd9ad2714af75a1abca927c2f
zhwei <zhwei.yes@gmail.com>
2015-12-11 12:16:29 +0800

    disable resize

--- public/assets/qn-file/upload.js
+++ public/assets/qn-file/upload.js
@@ -262,7 +262,6 @@ function Uploader($trigger, opts) {
 			dragdrop: false,     //开启可拖曳上传
 			auto_start: true,                 //选择文件后自动上传，若关闭需要自己绑定事件触发上传,
 			canSendBinary: true,
-			resize: opts.compress,
 			init: {
 				'FilesAdded': function (up, files) {
 					plupload.each(files, function (file) {
