ae817fb5072908b4645b86336225050b1a8557b2
Zhang Wei <zhwei@users.noreply.github.com>
2016-05-30 13:02:21 +0800

    Number Field：从rule中解析最大值最小值并应用在html层 (#38)
    * 加常用Validation函数
    
    * Number Field：从rule中解析最大值最小值并应用在html层

--- src/DataForm/Field/Number.php
+++ src/DataForm/Field/Number.php
@@ -13,6 +13,37 @@ class Number extends Field
     public $clause = "where";
     public $rule = ["numeric"];
 
+    private function addLimitsToAttributes()
+    {
+        $max = null;
+        $min = null;
+        foreach ($this->rule as $rule) {
+            switch (true) {
+                case preg_match('/^between:(-?\d+),(-?\d+)$/', $rule, $match):
+                    if ($match[1] || $match[1] == 0) {
+                        list($min, $max) = array_slice($match, -2);
+                    }
+                    break;
+
+                case preg_match('/^max:(-?\d+)$/', $rule, $match):
+                    $max = $match[1];
+                    break;
+
+                case preg_match('/^min:(-?\d+)$/', $rule, $match):
+                    $min = $match[1];
+                    break;
+            }
+        }
+
+        if (!is_null($max)) {
+            $this->attributes ['max'] = $max;
+        }
+
+        if (!is_null($min)) {
+            $this->attributes ['min'] = $min;
+        }
+    }
+
     public function build()
     {
         $output = "";
@@ -37,6 +68,7 @@ class Number extends Field
 
             case "create":
             case "modify":
+                $this->addLimitsToAttributes();
                 $output = Form::number($this->name, $this->value, $this->attributes);
                 break;
 
